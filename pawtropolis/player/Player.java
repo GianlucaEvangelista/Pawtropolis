66276629bf76cbec194c0898198aba4d18f0ab02
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-08 11:09:23 +0100

    Feat(Player): create Player class

---
+++ pawtropolis/player/Player.java
@@ -0,0 +1,34 @@
+package pawtropolis.player;
+
+public class Player {
+
+    private String name;
+    private int lifePoints;
+    private final Bag bag;
+
+    public Player(String name, int lifePoints) {
+        this.name = name;
+        this.lifePoints = lifePoints;
+        this.bag = new Bag();
+    }
+
+    public int getLifePoints() {
+        return lifePoints;
+    }
+
+    public void setLifePoints(int lifePoints) {
+        this.lifePoints = lifePoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
