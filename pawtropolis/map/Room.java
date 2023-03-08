86cabc70dec7f6d9809a70bcd5c89b6adef7a8a4
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-08 11:30:12 +0100

    Feat(Room): create Room class

---
+++ pawtropolis/map/Room.java
@@ -0,0 +1,52 @@
+package pawtropolis.map;
+import pawtropolis.player.Item;
+import pawtropolis.zoo.Animal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+
+    private String name;
+    private List<Item> items;
+    private List<Animal> animals;
+    private List<Room> adjacentRooms;
+
+    public Room(String name) {
+        this.name = name;
+        this.items = new ArrayList<>();
+        this.animals = new ArrayList<>();
+        this.adjacentRooms = new ArrayList<>(4);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Room> getAdjacentRooms() {
+        return adjacentRooms;
+    }
+
+    public void setAdjacentRooms(List<Room> adjacentRooms) {
+        this.adjacentRooms = adjacentRooms;
+    }
+}
