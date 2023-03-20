f5c042802f09d0dff46be3e20774e9cf6c57566f
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-20 17:19:39 +0100

    Refactor(CommandController): use Lombok annotations for getter, setter and constructor

--- src/main/java/pawtropolis/command/CommandController.java
+++ src/main/java/pawtropolis/command/CommandController.java
@@ -1,25 +1,16 @@
 package pawtropolis.command;
+import lombok.*;
 import pawtropolis.command.model.*;
 import pawtropolis.game.GameController;
 import pawtropolis.game.InputController;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class CommandController {
 
     private GameController gameController;
-
-    public CommandController(GameController gameController) {
-        this.gameController = gameController;
-    }
-
-    public GameController getGameController() {
-        return gameController;
-    }
-
-    public void setGameController(GameController gameController) {
-        this.gameController = gameController;
-    }
-
 
     public void executeCommand(GameController gameController) {
         boolean commandIsValid = false;
