de8b5fa463cbfbd5905ae5a1a9b0c455d3fcf541
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-20 17:19:08 +0100

    Refactor(Command): use Lombok annotations for constructor

--- src/main/java/pawtropolis/command/model/Command.java
+++ src/main/java/pawtropolis/command/model/Command.java
@@ -1,16 +1,14 @@
 package pawtropolis.command.model;
+import lombok.AllArgsConstructor;
 import pawtropolis.game.GameController;
 import java.util.List;
 
+@AllArgsConstructor
 public abstract class Command {
 
     protected final GameController gameController;
 
     protected static final String NOT_VALID_COMMAND = "Command isn't valid!";
-
-    protected Command(GameController gameController) {
-        this.gameController = gameController;
-    }
 
     public abstract boolean execute(GameController gameController, List<String> chosenCommand);
 }
