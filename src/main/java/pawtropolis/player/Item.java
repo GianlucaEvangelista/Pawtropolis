82ccd9c5919c0b68d2004461a1f61f636fa144f0
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-20 17:14:33 +0100

    Refactor(Item): use Lombok annotations for getter, setter and constructor

--- src/main/java/pawtropolis/player/Item.java
+++ src/main/java/pawtropolis/player/Item.java
@@ -1,38 +1,13 @@
 package pawtropolis.player;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Item {
 
     private String name;
     private String description;
     private int requiredSlots;
 
-    public Item(String name, String description, int requiredSlots) {
-        this.name = name;
-        this.description = description;
-        this.requiredSlots = requiredSlots;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getRequiredSlots() {
-        return requiredSlots;
-    }
-
-    public void setRequiredSlots(int requiredSlots) {
-        this.requiredSlots = requiredSlots;
-    }
 }
