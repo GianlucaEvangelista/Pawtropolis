7f6e46598c79d81ebdf24b39edefc3d2659f1d7d
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-20 17:12:12 +0100

    Refactor(Lion): use Lombok annotations for getter and setter

--- src/main/java/pawtropolis/zoo/model/Lion.java
+++ src/main/java/pawtropolis/zoo/model/Lion.java
@@ -1,6 +1,10 @@
 package pawtropolis.zoo.model;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class Lion extends Tailed {
 
     public Lion(String name, String favouriteFood, int age, LocalDate arrivalDate, double weight, double height, double tailLength) {
