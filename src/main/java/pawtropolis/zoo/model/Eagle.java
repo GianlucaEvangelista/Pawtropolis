e28b59a528ba254b4d0c6ea8fb20b6af8cb01efb
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-20 17:11:58 +0100

    Refactor(Eagle): use Lombok annotations for getter and setter

--- src/main/java/pawtropolis/zoo/model/Eagle.java
+++ src/main/java/pawtropolis/zoo/model/Eagle.java
@@ -1,9 +1,13 @@
 package pawtropolis.zoo.model;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class Eagle extends Winged {
 
-    public Eagle(String name, String favouriteFood, int age, LocalDate arrivalDate, double weight, double height, double wigspan) {
-        super(name, favouriteFood, age, arrivalDate, weight, height, wigspan);
+    public Eagle(String name, String favouriteFood, int age, LocalDate arrivalDate, double weight, double height, double wingspan) {
+        super(name, favouriteFood, age, arrivalDate, weight, height, wingspan);
     }
 }
