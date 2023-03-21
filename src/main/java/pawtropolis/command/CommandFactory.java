1a85707017f32f031d8f6ee4d500903116e21830
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-21 16:22:23 +0100

    Refactor(CommandFactory): use CommandType enum to generate the appropriate Command instance in the CommandFactory

--- src/main/java/pawtropolis/command/CommandFactory.java
+++ src/main/java/pawtropolis/command/CommandFactory.java
@@ -1,31 +1,17 @@
 package pawtropolis.command;
+import lombok.experimental.UtilityClass;
 import pawtropolis.command.model.*;
 import pawtropolis.game.GameController;
 import java.util.List;
 
+@UtilityClass
 public final class CommandFactory {
 
-    private CommandFactory() {}
-
     public static Command createCommand(GameController gameController, List<String> chosenCommand) {
-        if(chosenCommand.get(0) == null) {
+        if(chosenCommand.isEmpty() || chosenCommand.get(0) == null) {
             return null;
         }
-        switch (chosenCommand.get(0)) {
-            case "go":
-                return new GoCommand(gameController);
-            case "look":
-                return new LookCommand(gameController);
-            case "bag":
-                return new BagCommand(gameController);
-            case "get":
-                return new GetCommand(gameController);
-            case "drop":
-                return new DropCommand(gameController);
-            case "exit":
-                return new ExitCommand(gameController);
-            default:
-                return new UnknownCommand(gameController);
-        }
+        CommandType commandType = CommandType.convertStringToCommand(chosenCommand.get(0));
+        return commandType.createCommand(gameController);
     }
 }
