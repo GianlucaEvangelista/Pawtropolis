fb53c37048fb45b11c2e36b2a7772b9501582c55
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-03-28 16:31:18 +0200

    Refactor(CommandFactory): make CommandFactory a @Component with @Autowired constructor and add "commandMap" field

--- src/main/java/pawtropolis/command/CommandFactory.java
+++ src/main/java/pawtropolis/command/CommandFactory.java
@@ -1,20 +1,32 @@
 package pawtropolis.command;
-import lombok.experimental.UtilityClass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import pawtropolis.command.model.*;
 import pawtropolis.game.GameController;
 
-@UtilityClass
-public final class CommandFactory {
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@Component
+public class CommandFactory {
+
+    private static final Map<CommandType, Command> commandMap = new EnumMap<>(CommandType.class);
+
+    @Autowired
+    private CommandFactory(List<Command> commandList) {
+        commandList.forEach(command -> {
+            CommandType commandType = CommandType.fromCommandClass(command.getClass());
+            commandMap.put(commandType, command);
+        });
+    }
 
     public static Command createCommand(GameController gameController, String commandNameInput) {
-        try {
-            if (commandNameInput == null) {
-                return CommandType.UNKNOWN.getCommandClass().getConstructor(GameController.class).newInstance(gameController);
-            }
-            CommandType commandType = CommandType.fromString(commandNameInput);
-            return commandType.getCommandClass().getConstructor(GameController.class).newInstance(gameController);
-        } catch (ReflectiveOperationException e){
-            return null;
-        }
+        CommandType commandType = commandNameInput == null ? CommandType.UNKNOWN : CommandType.fromString(commandNameInput);
+        return commandMap.get(commandType);
     }
 }
