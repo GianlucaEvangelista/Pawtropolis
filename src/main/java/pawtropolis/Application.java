07ca28c7c21acdcc635ebc2938db13a31eba8fb1
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-05-17 09:53:17 +0200

    Refactor(Application): replace @Component annotation with @SpringBootApplication annotation

--- src/main/java/pawtropolis/Application.java
+++ src/main/java/pawtropolis/Application.java
@@ -3,12 +3,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pawtropolis.config.AppConfiguration;
 import pawtropolis.game.GameController;
 import pawtropolis.zoo.*;
 
-@Component
+@SpringBootApplication
 public class Application implements ApplicationRunner {
 
     private final GameController gameController;
