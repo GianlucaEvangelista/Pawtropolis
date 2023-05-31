c1d0c6611cd37a11f1fde9e4bbc0e95f78e69938
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-05-31 15:32:41 +0200

    Feat(Door): create new Door class

---
+++ src/main/java/pawtropolis/map/model/Door.java
@@ -0,0 +1,24 @@
+package pawtropolis.map.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import pawtropolis.game.model.Item;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Door {
+
+    private boolean isOpen;
+
+    private Item keyItem;
+
+    public boolean openTheDoor(Item item) {
+        if(this.keyItem == item) {
+            this.isOpen = true;
+            return true;
+        }
+        System.out.println("This isn't the right item to open this door!");
+        return false;
+    }
+}
