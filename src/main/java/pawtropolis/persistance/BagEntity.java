86f091535c85270f1507fd1ad5b0a9f9de0404f8
plabriola <paololabriola02@gmail.com>
2023-06-08 16:25:03 +0200

    Feat(BagEntity): create DB entity class for bags

---
+++ src/main/java/pawtropolis/persistance/BagEntity.java
@@ -0,0 +1,29 @@
+package pawtropolis.persistance;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "bags")
+@Getter
+@Setter
+@NoArgsConstructor
+public class BagEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "available_slots")
+    @NotNull
+    private Integer availableSlots;
+    @ManyToMany
+    @JoinTable(
+            name = "items_in_bag",
+            joinColumns = @JoinColumn(name = "bag_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<ItemEntity> itemEntities;
+}
