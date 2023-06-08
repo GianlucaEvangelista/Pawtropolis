0907592b40b2f50930e1f25012204fe09314fde4
plabriola <paololabriola02@gmail.com>
2023-06-08 16:27:26 +0200

    Feat(TailedEntity): create DB entity class for tailed animals

---
+++ src/main/java/pawtropolis/persistance/TailedEntity.java
@@ -0,0 +1,24 @@
+package pawtropolis.persistance;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tail_length")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TailedEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "tail_length")
+    @NotNull
+    private Double tailLength;
+    @OneToOne(targetEntity = AnimalEntity.class)
+    @PrimaryKeyJoinColumn(referencedColumnName = "id")
+    @NotNull
+    private AnimalEntity animalEntity;
+}
