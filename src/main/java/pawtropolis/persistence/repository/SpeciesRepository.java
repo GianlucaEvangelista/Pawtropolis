70ba44edeb1e124568b080a3d073158b7e611c35
plabriola <paololabriola02@gmail.com>
2023-06-19 11:52:07 +0200

    Feat(SpeciesRepository): create SpeciesRepository

---
+++ src/main/java/pawtropolis/persistence/repository/SpeciesRepository.java
@@ -0,0 +1,9 @@
+package pawtropolis.persistence.repository;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pawtropolis.persistence.model.SpeciesEntity;
+@Repository
+public interface SpeciesRepository extends JpaRepository<SpeciesEntity, Integer> {
+    SpeciesEntity findByName(@NonNull String name);
+}
