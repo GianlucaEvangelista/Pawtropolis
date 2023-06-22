24c802432ea82e2fc114f82591fbfeeca0b691c8
plabriola <paololabriola02@gmail.com>
2023-06-22 15:28:15 +0200

    Refactor(RoomRepository): create "findByName" method

--- src/main/java/pawtropolis/persistence/repository/RoomRepository.java
+++ src/main/java/pawtropolis/persistence/repository/RoomRepository.java
@@ -3,4 +3,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pawtropolis.persistence.model.RoomEntity;
 
 public interface RoomRepository extends JpaRepository<RoomEntity, Integer> {
+    RoomEntity findByName(String name);
 }
