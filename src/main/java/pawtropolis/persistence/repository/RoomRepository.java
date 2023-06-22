effdfab0ec0a7af2696de2830098408ab47e51a9
plabriola <paololabriola02@gmail.com>
2023-06-22 16:37:39 +0200

    Refactor(RoomRepository): add query to delete item in room

--- src/main/java/pawtropolis/persistence/repository/RoomRepository.java
+++ src/main/java/pawtropolis/persistence/repository/RoomRepository.java
@@ -1,7 +1,13 @@
 package pawtropolis.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pawtropolis.persistence.model.RoomEntity;
 
 public interface RoomRepository extends JpaRepository<RoomEntity, Integer> {
     RoomEntity findByName(String name);
+    @Modifying
+    @Query(value = "DELETE FROM items_in_room WHERE room_id = (SELECT id FROM rooms WHERE name = :roomName) AND item_id = (SELECT id FROM items WHERE name = :itemName)", nativeQuery = true)
+    void deleteItemInRoom(@Param("roomName") String roomName, @Param("itemName") String itemName);
 }
