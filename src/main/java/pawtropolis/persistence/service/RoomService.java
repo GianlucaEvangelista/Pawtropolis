732f95821d7c4916a2d7a0c152cd89c4de697bd1
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-06-23 12:54:38 +0200

    Refactor(RoomService): add "itemRepository" attribute and create "getItemEntityFromRoom" method

--- src/main/java/pawtropolis/persistence/service/RoomService.java
+++ src/main/java/pawtropolis/persistence/service/RoomService.java
@@ -2,11 +2,13 @@ package pawtropolis.persistence.service;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pawtropolis.game.model.Item;
 import pawtropolis.map.model.Room;
 import pawtropolis.persistence.marshaller.RoomMarshaller;
+import pawtropolis.persistence.model.ItemEntity;
 import pawtropolis.persistence.model.RoomEntity;
+import pawtropolis.persistence.repository.ItemRepository;
 import pawtropolis.persistence.repository.RoomRepository;
+import java.util.Optional;
 
 @Service
 public class RoomService {
@@ -14,12 +16,14 @@ public class RoomService {
     private final RoomRepository roomRepository;
     private final RoomLinkService roomLinkService;
     private final RoomMarshaller roomMarshaller;
+    private final ItemRepository itemRepository;
 
     @Autowired
-    public RoomService(RoomRepository roomRepository, RoomLinkService roomLinkService, RoomMarshaller roomMarshaller) {
+    public RoomService(RoomRepository roomRepository, RoomLinkService roomLinkService, RoomMarshaller roomMarshaller, ItemRepository itemRepository) {
         this.roomRepository = roomRepository;
         this.roomLinkService = roomLinkService;
         this.roomMarshaller = roomMarshaller;
+        this.itemRepository = itemRepository;
     }
 
     public Room getRoomById(int id) {
@@ -28,6 +32,12 @@ public class RoomService {
         Room room = roomMarshaller.toRoom(roomEntity);
         room.setAdjacentRooms(roomLinkService.getRoomLinkByRoomEntityId(id));
         return room;
+    }
+
+    public ItemEntity getItemEntityFromRoom(Room room, String itemName) {
+        Integer itemId = roomRepository.getItemIdFromRoom(room.getName(), itemName);
+        Optional<ItemEntity> optionalItemEntity = itemRepository.findById(itemId);
+        return optionalItemEntity.orElse(null);
     }
 
     public Room getRoomByName(String name) {
