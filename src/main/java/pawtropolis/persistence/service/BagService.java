4aad59418a1746bfff20312722e67662ba5aebaa
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-06-26 15:11:54 +0200

    Refactor(BagService): add "itemRepository" field and create "getItemEntity" and "removeItem" methods

--- src/main/java/pawtropolis/persistence/service/BagService.java
+++ src/main/java/pawtropolis/persistence/service/BagService.java
@@ -8,6 +8,7 @@ import pawtropolis.persistence.marshaller.BagMarshaller;
 import pawtropolis.persistence.model.BagEntity;
 import pawtropolis.persistence.model.ItemEntity;
 import pawtropolis.persistence.repository.BagRepository;
+import pawtropolis.persistence.repository.ItemRepository;
 import java.util.List;
 
 @Service
@@ -16,12 +17,14 @@ public class BagService {
     private final BagRepository bagRepository;
     private final BagMarshaller bagMarshaller;
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
 
     @Autowired
-    public BagService(BagRepository bagRepository, BagMarshaller bagMarshaller, ItemService itemService) {
+    public BagService(BagRepository bagRepository, BagMarshaller bagMarshaller, ItemService itemService, ItemRepository itemRepository) {
         this.bagRepository = bagRepository;
         this.bagMarshaller = bagMarshaller;
         this.itemService = itemService;
+        this.itemRepository = itemRepository;
     }
 
     public Bag getBagById(int id) {
@@ -51,5 +54,17 @@ public class BagService {
     public List<Item> getItems(BagEntity bagEntity) {
         List<Integer> itemsId = bagRepository.getItemsIdFromBag(bagEntity.getId());
         return itemsId.stream().map(itemService::getItemById).toList();
+    }
+
+    public ItemEntity getItemEntity(BagEntity bagEntity, String itemName) {
+        Integer itemEntityId = bagRepository.getItemIdFromBag(bagEntity.getId(), itemName);
+        return itemRepository.findById(itemEntityId).orElse(null);
+    }
+
+    @Transactional
+    public void removeItem(BagEntity bagEntity, ItemEntity itemEntity) {
+        bagEntity.getItemEntities().removeIf(i -> i.getName().equals(itemEntity.getName()));
+        bagEntity.setAvailableSlots(bagEntity.getAvailableSlots() + itemEntity.getRequiredSlots());
+        bagRepository.save(bagEntity);
     }
 }
