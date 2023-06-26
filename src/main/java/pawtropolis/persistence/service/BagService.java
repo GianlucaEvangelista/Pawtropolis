68a3d2e63be5514d171cc5385d761fb6ca892748
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-06-26 11:11:09 +0200

    Refactor(BagService): add "itemService" field and create "getItems" method

--- src/main/java/pawtropolis/persistence/service/BagService.java
+++ src/main/java/pawtropolis/persistence/service/BagService.java
@@ -3,21 +3,25 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pawtropolis.game.model.Bag;
+import pawtropolis.game.model.Item;
 import pawtropolis.persistence.marshaller.BagMarshaller;
 import pawtropolis.persistence.model.BagEntity;
 import pawtropolis.persistence.model.ItemEntity;
 import pawtropolis.persistence.repository.BagRepository;
+import java.util.List;
 
 @Service
 public class BagService {
 
     private final BagRepository bagRepository;
     private final BagMarshaller bagMarshaller;
+    private final ItemService itemService;
 
     @Autowired
-    public BagService(BagRepository bagRepository, BagMarshaller bagMarshaller) {
+    public BagService(BagRepository bagRepository, BagMarshaller bagMarshaller, ItemService itemService) {
         this.bagRepository = bagRepository;
         this.bagMarshaller = bagMarshaller;
+        this.itemService = itemService;
     }
 
     public Bag getBagById(int id) {
@@ -42,5 +46,10 @@ public class BagService {
 
     public boolean isThereEnoughSpace(BagEntity bagEntity, ItemEntity itemEntityToAdd) {
         return bagEntity.getAvailableSlots() >= itemEntityToAdd.getRequiredSlots();
+    }
+
+    public List<Item> getItems(BagEntity bagEntity) {
+        List<Integer> itemsId = bagRepository.getItemsIdFromBag(bagEntity.getId());
+        return itemsId.stream().map(itemService::getItemById).toList();
     }
 }
