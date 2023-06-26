2a6807f13b49dbe4af6d5ceba65c043fceec35d5
plabriola <paololabriola02@gmail.com>
2023-06-26 15:55:54 +0200

    Refactor(DoorService): create "isTheRightKey" method

--- src/main/java/pawtropolis/persistence/service/DoorService.java
+++ src/main/java/pawtropolis/persistence/service/DoorService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import pawtropolis.map.model.Door;
 import pawtropolis.persistence.marshaller.DoorMarshaller;
 import pawtropolis.persistence.model.DoorEntity;
+import pawtropolis.persistence.model.ItemEntity;
 import pawtropolis.persistence.repository.DoorRepository;
+import pawtropolis.persistence.repository.ItemRepository;
 import java.util.Optional;
 
 @Service
@@ -13,9 +15,8 @@ public class DoorService {
     private final DoorRepository doorRepository;
 
     private final DoorMarshaller doorMarshaller;
-
     @Autowired
-    public DoorService(DoorRepository doorRepository, DoorMarshaller doorMarshaller) {
+    public DoorService(DoorRepository doorRepository, DoorMarshaller doorMarshaller, ItemRepository itemRepository) {
         this.doorRepository = doorRepository;
         this.doorMarshaller = doorMarshaller;
     }
@@ -31,5 +32,12 @@ public class DoorService {
         Optional<DoorEntity> optionalDoorEntity = doorRepository.findById(door.getId());
         optionalDoorEntity.ifPresent(doorEntity -> doorEntity.setLocked(false));
         return optionalDoorEntity.filter(doorEntity -> !doorEntity.getLocked()).isPresent();
+    }
+
+    public boolean isTheRightKey(Door door, ItemEntity item) {
+        DoorEntity doorEntity = doorRepository.findById(door.getId()).orElse(new DoorEntity());
+        return doorEntity.getKeyItemEntity().getName().equals(item.getName()) &&
+                doorEntity.getKeyItemEntity().getDescription().equals(item.getDescription()) &&
+                doorEntity.getKeyItemEntity().getRequiredSlots().equals(item.getRequiredSlots());
     }
 }
