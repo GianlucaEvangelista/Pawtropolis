5de22df08628b447ffbb581e0955ddba96771e2a
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-06-26 15:09:04 +0200

    Refactor(BagRepository): create "getItemIdFromBag" method

--- src/main/java/pawtropolis/persistence/repository/BagRepository.java
+++ src/main/java/pawtropolis/persistence/repository/BagRepository.java
@@ -9,4 +9,7 @@ public interface BagRepository extends JpaRepository<BagEntity, Integer> {
 
     @Query(value = "SELECT item_id FROM items_in_bag WHERE bag_id = (SELECT id FROM bags WHERE id = :bagId)", nativeQuery = true)
     List<Integer> getItemsIdFromBag(@Param("bagId") Integer bagId);
+
+    @Query(value = "SELECT item_id FROM items_in_bag WHERE bag_id = (SELECT id FROM bags WHERE id = :bagId) AND item_id = (SELECT id FROM items WHERE name = :itemName)", nativeQuery = true)
+    Integer getItemIdFromBag(@Param("bagId") Integer bagId, @Param("itemName") String itemName);
 }
