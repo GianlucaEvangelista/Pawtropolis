e1f2944500045e9095cd0e94261baf2631d5560e
plabriola <paololabriola02@gmail.com>
2023-06-26 16:56:23 +0200

    Chore(Room): remove unused methods

--- src/main/java/pawtropolis/map/model/Room.java
+++ src/main/java/pawtropolis/map/model/Room.java
@@ -23,28 +23,6 @@ public class Room {
         this.adjacentRooms = new EnumMap<>(Direction.class);
     }
 
-    public void addAnimal(Animal animal) {
-        animals.add(animal);
-    }
-
-    public void removeAnimal(Animal animal) {
-        animals.removeIf(a -> a.getName().equals(animal.getName()));
-    }
-
-    public void addItem(Item item) {
-        items.add(item);
-    }
-
-    public void removeItem(Item item) {
-        items.removeIf(i -> i.getName().equals(item.getName()));
-    }
-
-    public void addAdjacentRoom(Room adjacentRoom, Door door, Direction direction) {
-            if (adjacentRoom != null && !adjacentRooms.containsKey(direction) && adjacentRoom.linkAdjacentRoom(this, door, direction.reverseDirection())) {
-                adjacentRooms.put(direction, new Pair<>(adjacentRoom, door));
-            }
-    }
-
     public boolean linkAdjacentRoom(Room room, Door door, Direction reverseDirection) {
         Pair<Room, Door> roomAndDoorInReverseDirection = adjacentRooms.computeIfAbsent(reverseDirection, key -> new Pair<>(room, door));
         return roomAndDoorInReverseDirection.getFirst() ==  room && roomAndDoorInReverseDirection.getSecond() == door;
@@ -54,16 +32,8 @@ public class Room {
         return adjacentRooms.get(direction).getFirst();
     }
 
-    public Door getAdjacentRoomDoor(Direction direction) {
-        return adjacentRooms.get(direction).getSecond();
-    }
-
     public boolean containsItem(String itemName) {
         return getItems().stream().anyMatch(item -> item.getName().equals(itemName));
-    }
-
-    public Item getItem (String itemName) {
-        return getItems().stream().filter(item -> item.getName().equals(itemName)).findFirst().orElse(null);
     }
 
     public String getItemsNames() {
