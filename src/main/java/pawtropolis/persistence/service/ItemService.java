50721240b5a6761193440fa36ec53364de1126bb
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-06 17:12:49 +0200

    Refactor(ItemService): remove unused methods, fields and imports

--- src/main/java/pawtropolis/persistence/service/ItemService.java
+++ src/main/java/pawtropolis/persistence/service/ItemService.java
@@ -1,8 +1,6 @@
 package pawtropolis.persistence.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pawtropolis.game.model.Item;
-import pawtropolis.persistence.marshaller.ItemMarshaller;
 import pawtropolis.persistence.model.ItemEntity;
 import pawtropolis.persistence.repository.ItemRepository;
 import java.util.Optional;
@@ -11,18 +9,9 @@ import java.util.Optional;
 public class ItemService {
     private final ItemRepository itemRepository;
 
-    private final ItemMarshaller itemMarshaller;
-
     @Autowired
-    public ItemService(ItemRepository itemRepository, ItemMarshaller itemMarshaller) {
+    public ItemService(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
-        this.itemMarshaller = itemMarshaller;
-    }
-
-    public Item getItemById(int id) {
-        ItemEntity itemEntity = itemRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Item not found"));
-        return itemMarshaller.toItem(itemEntity);
     }
 
     public ItemEntity getItemEntityById(Integer id) {
