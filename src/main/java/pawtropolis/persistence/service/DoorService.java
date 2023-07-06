7e3f753757abfc8658b2e6a7831b5f2bc022cc1a
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-06 17:12:33 +0200

    Refactor(DoorService): remove unused methods, fields and imports

--- src/main/java/pawtropolis/persistence/service/DoorService.java
+++ src/main/java/pawtropolis/persistence/service/DoorService.java
@@ -3,29 +3,17 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pawtropolis.map.model.Door;
-import pawtropolis.persistence.marshaller.DoorMarshaller;
 import pawtropolis.persistence.model.DoorEntity;
 import pawtropolis.persistence.repository.DoorRepository;
-import pawtropolis.persistence.repository.ItemRepository;
 import java.util.Optional;
 
 @Service
 public class DoorService {
     private final DoorRepository doorRepository;
 
-    private final DoorMarshaller doorMarshaller;
-
     @Autowired
-    public DoorService(DoorRepository doorRepository, DoorMarshaller doorMarshaller, ItemRepository itemRepository) {
+    public DoorService(DoorRepository doorRepository) {
         this.doorRepository = doorRepository;
-        this.doorMarshaller = doorMarshaller;
-    }
-
-    public Door getDoorById(int id) {
-        DoorEntity doorEntity = doorRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Door not found"));
-
-        return doorMarshaller.toDoor(doorEntity);
     }
 
     public DoorEntity getDoorEntityById(Integer id) {
