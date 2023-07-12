0817516d8a5b841a2fc82e10b2c251cf3c474cff
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2023-07-12 12:43:26 +0200

    Feat(PlayerRepository): add "getMapIdFromPlayerId" query

--- src/main/java/pawtropolis/persistence/repository/PlayerRepository.java
+++ src/main/java/pawtropolis/persistence/repository/PlayerRepository.java
@@ -1,6 +1,11 @@
 package pawtropolis.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pawtropolis.persistence.model.PlayerEntity;
 
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Integer> {
+
+    @Query(value = "SELECT map_id FROM games WHERE player_id = :playerId", nativeQuery = true)
+    Integer getMapIdFromPlayerId(@Param("playerId") Integer playerId);
 }
